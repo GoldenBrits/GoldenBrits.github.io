7f0dbd0d5b031ca751d2181b14adb84239006d9a
GoldenBrits <goldenbrits101@gmail.com>
2022-01-02 21:13:13 +0000

    Update Christmas
    Overhauled the Java for the Login Page + a few other minor changes

---
+++ Login_Java(OLD).js
@@ -0,0 +1,43 @@
+var Checkbox = false;
+//brings the text back if no text is entered into the Username and Password area
+function TextBack(Name)
+{
+var Output = document.getElementById(Name);
+var Content = document.getElementById(Name).value;
+if (Content == '' )
+	{
+		if (Name == 'Login_Username')
+		{
+			Output.value="Example John Smith";
+		}
+		else if (Name == 'Login_Password')
+		{
+			Output.value="Example 6fI012/@;";
+		}			
+	} 
+}
+
+function TextKeep(Name)
+{
+var Output = document.getElementById(Name);
+var Content = document.getElementById(Name).value;
+if (Content == '' || Content == 'Example 6fI012/@;' || Content == 'Example John Smith')
+{
+	Output.value="";
+}
+}
+
+function Bool()
+{
+	Checkbox= !Checkbox;
+	var Inp = document.getElementById(Login_Password);
+	if (!Checkbox)
+	{
+		Login_Password.type = "password";
+	}
+	else
+	{
+		Login_Password.type = "text";
+	}
+}
+//Dynamically changes the type of the box containing the password from *s and text, it also allows the user to tick a check box to show the password  
--- Login_Java.js
+++ Login_Java.js
@@ -1,44 +1,56 @@
 var Checkbox = false;
-//brings the text back if no text is entered into the Username and Password area
-function TextBack(Name)
+
+
+function TextBackUN(Name)
 {
 var Output = document.getElementById(Name);
 var Content = document.getElementById(Name).value;
 if (Content == '')
-	{
-		if (Name == 'Login_Username')
-		{
-			Output.value="Example John Smith";
-		}
-		else if (Name == 'Login_Password')
-		{
-			Output.value="Example 6fI012/@;";
-		}			
-	} 
+	{		
+			Output.value="Example John Smith";		
+    }
 }
 
-function TextKeep(Name)
+
+function TextKeepUN(Name)
 {
 var Output = document.getElementById(Name);
 var Content = document.getElementById(Name).value;
-if (Content == '' || Content == 'Example 6fI012/@;' || Content == 'Example John Smith')
+if (Content == '' || Content == 'Example John Smith')
 {
 	Output.value="";
 }
 }
 
-function Bool()
+function TextBackPW(Name)
 {
-	Checkbox= !Checkbox;
-	var Inp = document.getElementById(Login_Password);
-	if (!Checkbox)
-	{
-		Login_Password.type = "password";
- 
-	}
-	else
-	{
-		Login_Password.type = "text";
-	}
+    {
+        var Output = document.getElementById(Name);
+        var Content = document.getElementById(Name).value;
+        if (Content == '')
+            {		
+                    Output.value="Example 6fI012/@;";		
+            } 
+        } 
 }
-//Dynamically changes the type of the box containing the password from *s and text, it also allows the user to tick a check box to show the password  
+
+
+function TextKeepPW(Name)
+{    
+    var Output = document.getElementById(Name);
+    var Content = document.getElementById(Name).value;
+    if (Content == '' || Content == 'Example 6fI012/@;')
+    {
+        Output.value="";
+    }
+    
+    else if (!Checkbox)
+    {
+        Login_Password.type = "password";
+    }
+    
+    else if (Checkbox)
+    {
+        Login_Password.type = "text";
+    }
+}
