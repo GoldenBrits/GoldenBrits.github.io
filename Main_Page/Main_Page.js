8c4a5eae959f6e4fb6ec5302ba55083920e1c958
GoldenBrits <94836731+GoldenBrits@users.noreply.github.com>
2022-02-11 16:21:42 +0000

    Big Cheese Update 1.7.5.3
    Added some code from a website to speed up the devolpment of my background

--- Main_Page/Main_Page.js
+++ Main_Page/Main_Page.js
@@ -44,18 +44,95 @@ switch(Requested)
 }
 
 
-function Background
+//Credit to https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations for the code
+function Background()
 {
-  var Canvas = document.getElementById("Canvas");
-  var Brush = Canvas.getContext("2d");
-  var Body = document.getElementById("Body");
-  var Width = Canvas.clientwidth;
+var img = new Image();
+
+
   
-  var Gradient = Brush.createLinearGradient(0, 0, Width, 0);
-  Gradient.addColourStop(0, rgb());
-  Gradient.addColourStop(1, rbg());
-  
+img.src = '../Assets/MainPage_Backgorund.png';
+var CanvasXSize = get.ElementById('Canvas').clientwidth;
+var CanvasYSize = get.ElementById('Canvas').clientheight;
+var speed = 30; // lower is faster
+var scale = 1.05;
+var y = -4.5; // vertical offset
+
+// Main program
+
+var dx = 0.75;
+var imgW;
+var imgH;
+var x = 0;
+var clearX;
+var clearY;
+var ctx;
+
+img.onload = function() {
+    imgW = img.width * scale;
+    imgH = img.height * scale;
+
+    if (imgW > CanvasXSize) {
+        // image larger than canvas
+        x = CanvasXSize - imgW;
+    }
+    if (imgW > CanvasXSize) {
+        // image width larger than canvas
+        clearX = imgW;
+    } else {
+        clearX = CanvasXSize;
+    }
+    if (imgH > CanvasYSize) {
+        // image height larger than canvas
+        clearY = imgH;
+    } else {
+        clearY = CanvasYSize;
+    }
+
+    // get canvas context
+    ctx = document.getElementById('canvas').getContext('2d');
+
+    // set refresh rate
+    return setInterval(draw, speed);
 }
 
+  
+function draw() {
+    ctx.clearRect(0, 0, clearX, clearY); // clear the canvas
+  
+    //Set the colour chang up here[][][][][][][][][][][][][][[][][][[][][][][][][[][][][][][][][][][][][][[][][][][][][][][][][][][[][[][][]
+    var = new date;
 
+    // if image is <= Canvas Size
+    if (imgW <= CanvasXSize) {
+        // reset, start from beginning
+        if (x > CanvasXSize) {
+            x = -imgW + x;
+        }
+        // draw additional image1
+        if (x > 0) {
+            ctx.drawImage(img, -imgW + x, y, imgW, imgH);
+        }
+        // draw additional image2
+        if (x - imgW > 0) {
+            ctx.drawImage(img, -imgW * 2 + x, y, imgW, imgH);
+        }
+    }
 
+    // image is > Canvas Size
+    else {
+        // reset, start from beginning
+        if (x > (CanvasXSize)) {
+            x = CanvasXSize - imgW;
+        }
+        // draw additional image
+        if (x > (CanvasXSize-imgW)) {
+            ctx.drawImage(img, x - imgW + 1, y, imgW, imgH);
+        }
+    }
+    // draw image
+    ctx.drawImage(img, x, y,imgW, imgH);
+    // amount to move
+    x += dx;
+}
+}
