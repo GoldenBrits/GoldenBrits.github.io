734d6e9effe31c12498b572a758f955ef97cbc5d
GoldenBrits <goldenbrits101@gmail.com>
2022-03-20 21:53:32 +0000

    The Weekend Update
    Done a bunch of stuff Check the change logs to see in more detail

---
+++ SignUp_Page/SignUp.js
@@ -0,0 +1,220 @@
+var Checkbox = false;
+
+var ErrorG = false;
+var ErrorU = false;
+var ErrorP = false;
+
+
+function TextBackUN(Name)
+{
+	var Output = document.getElementById(Name);
+	var Content = document.getElementById(Name).value;
+	if (Content == '')
+		{		
+				Output.value="Example John Smith";		
+	    }
+}
+
+
+function TextKeepUN(Name)
+{
+	var Output = document.getElementById(Name);
+	var Content = document.getElementById(Name).value;
+	if (Content == '' || Content == 'Example John Smith')
+	{
+		Output.value="";
+	}
+}
+
+
+function TextBackPW(Name)
+{   
+    var Output = document.getElementById(Name);
+    var Content = document.getElementById(Name).value;
+	if (Content == '' || Content == 'Example 6fI012/@;')
+   	{
+        Output.value="Example 6fI012/@;";
+		SignUp_Password.type = "text";
+    	}
+	else
+	{
+		SignUp_Password.type = "password";	
+	}
+}
+
+
+function TextKeepPW(Name)
+{    
+    var Output = document.getElementById(Name);
+    var Content = document.getElementById(Name).value;
+    if (Content == '' || Content == 'Example 6fI012/@;')
+    {
+        Output.value="";
+		SignUp_Password.type = "text";
+    }
+    
+    else if (!Checkbox)
+    {
+        SignUp_Password.type = "password";
+    }
+    
+    else if (Checkbox)
+    {
+        SignUp_Password.type = "text";
+    }
+}
+
+
+function TextBackEM(Name)
+{
+	var Output = document.getElementById(Name);
+	var Content = document.getElementById(Name).value;
+	if (Content == '')
+		{		
+				Output.value="Example JohnSmith@mail.com";		
+	    }
+}
+
+
+function TextKeepEM(Name)
+{
+	var Output = document.getElementById(Name);
+	var Content = document.getElementById(Name).value;
+	if (Content == '' || Content == 'Example JohnSmith@mail.com')
+	{
+		Output.value="";
+	}
+}
+
+
+function TextBackPN(Name)
+{
+	var Output = document.getElementById(Name);
+	var Content = document.getElementById(Name).value;
+	if (Content == '')
+		{		
+				Output.value="Example 07794522365";	
+	    }
+}
+
+
+function TextKeepPN(Name)
+{
+	var Output = document.getElementById(Name);
+	var Content = document.getElementById(Name).value;
+	if (Content == '' || Content == 'Example 07794522365')
+	{
+		Output.value="";
+	}
+}
+
+
+function SignUp_Attempt()
+{
+	var ContentUN = document.getElementById('SignUp_Username').value;
+    var ContentPW = document.getElementById('SignUp_Password').value;
+    var ContentPN = document.getElementById('SignUp_Phone').value;
+    var ContentEM = document.getElementById('SignUp_Email').value;
+	
+	ErrorG = false;
+	ErrorU = false;
+	ErrorP = false;
+
+	if (ContentUN == "Example John Smith" || ContentUN == "")
+	{
+		ErrorU = true;
+	}
+
+	if (ContentPW == "Example 6fI012/@;" || ContentPW == "")
+	{
+		ErrorP = true;
+	}
+	if (ErrorU == false && ErrorP == false)
+	{
+        //Regular Experssions that check for certain paramaters
+        var Cap = ContentPW.match(/[A-Z]/);//Checks for Capital Letters in Password
+		var Lower = ContentPW.match(/[a-z]/);//Checks for Lower Case Letters in Password
+		var Num = ContentPW.match(/[\d]/);//Checks for a Number in Password
+        var Spec = ContentPW.match(/[`¬!"£$%^&*(_)+=<>,.?/:;@'{[}#~}-]/);//Checks for a Special Character in Password
+        var Email = ContentEM.match(/[@]/);
+        var Phone = ContentPN.match(/[07]/);
+        var PhoneL = ContentPN.length;
+        
+		
+		var ErrorString = "Error(s) found<br>";
+
+		if (Cap == null)
+		{			
+			ErrorG = true;
+			ErrorString += "No Capital Letter in Psssword<br>"; 
+		}
+
+		if (Lower == null)
+		{			
+			ErrorG = true;
+			ErrorString += "No Lower Case Letter in Psssword<br>";
+		}
+
+		if (Num == null)
+		{
+			ErrorG = true;
+			ErrorString += "No Number in Psssword<br>";
+		}
+
+		if (Spec == null)
+		{
+			ErrorG = true;
+			ErrorString += "No Special Character in Password<br>";
+		}
+
+        if(ContentEM == null || ContentEM == 'Example JohnSmith@mail.com')
+        {
+            ErrorG = true;
+            ErrorString += "No Email Entered<br>";
+        }
+        else if(Email == null)
+        {
+            ErrorG = true;
+            ErrorString += "Not Valid Email Format<br>";
+        }
+        
+        if(PhoneL != 11 || Phone == null || isNaN(ContentPN) == true)
+        {
+            ErrorG = true;
+            ErrorString+="Not Valid Phone Number Format<br>"
+        }
+
+
+		if (ErrorG == true)
+		{
+			document.getElementById('Password_Info').innerHTML = ErrorString;
+		}
+		else 
+		{
+            //check database and stuff
+            /*
+			document.getElementById('signupButton').style.visibility = "visible";
+			document.getElementById('SignUp_Submit_Button').style.visibility = "collapse";
+            document.getElementById('alertDia').style.visibility = "visible";#
+            */
+           fetch(SignUp.php);
+		}
+	}
+	else
+	{
+		if (ErrorU == true && ErrorP == true)
+		{
+			document.getElementById('Password_Info').innerHTML = "Error with Username<br>Error with Password<br>Error found Default value or Empty";
+		}
+
+		else if (ErrorP == true)
+		{
+			document.getElementById('Password_Info').innerHTML = "Error with Password<br>Error found Default value or Empty";
+		}
+
+		else
+		{
+			document.getElementById('Password_Info').innerHTML = "Error with Username<br>Error found Default value or Empty";
+		}
+	}
+}
---
+++ SignUp_Page/SignUp.php
@@ -0,0 +1,25 @@
+<?php
+
+//Temporary cookie storage = session (variables)
+session_start();
+
+$databasestring = mysqli_connect('localhost', 'root', '', 'mathstutordevon');
+
+$username =  mysql_real_escape_string($_POST['SignUp_Username']); //injection proof
+$password = mysql_real_escape_string($_POST['SignUp_Password']);
+$email = mysql_real_escape_string($_POST['SignUp_Email']);
+$phone = mysql_real_escape_string($_POST['SignUp_Phone']);
+
+$hashedPassword = md5($password);//Hashing the Password
+
+$insertUserQuery = "INSERT INTO user_data (UserName, Password, Email, Phone, IsTeacher) VALUES('$username', '$hashedPassword', '$email', '$phone', 0)";//The SQL statment that inserts the data into the database
+
+mysqli_query($databasestring, $insertUserQuery);
+
+$_SESSION['username'] = $username;
+$_SESSION['email'] = $email;
+$_SESSION['isteacher'] = false;
+
+header('location: ../index.html');//takes the User to the designated file aka further into the website
+
+?>
