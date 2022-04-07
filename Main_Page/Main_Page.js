3cc0fc6b34726bc4af18e6337a07ebe3b751b9fd
GoldenBrits <94836731+GoldenBrits@users.noreply.github.com>
2022-04-07 09:27:56 +0100

    Added some more comments

--- Main_Page/Main_Page.js
+++ Main_Page/Main_Page.js
@@ -1,18 +1,3 @@
-/*
-window.addEventListener('resize', function(event)
-{
-  let Canvas = document.querySelector("#CanvasHolder");
-  var Body = document.getElementById("Canvas");
-  var Width = Canvas.clientWidth;
-  var Height = Canvas.clientHeight;
-
-  Body.setAttribute('width', Width);
-  Body.setAttribute('height', Height);
-  console.log(Width, Height);
-}
-);
-*/
-
 var MainPage = "Mathematics is vital in our modern life, it surrounds us everywhere and is the key subject that opens many doors.  As our world becomes more and more technology based so the need for mathematical skills becomes more and more important.<br><br>You may be a student struggling with exams and day to day maths lessons, or an adult trying to cope with numeracy in the workplace, I am here to assist and help.<br><br>I am an enthusiastic and experienced maths tutor, tutoring all age groups from primary school up to GCSE standard.  With clear and concise explanations I aim to make maths more understandable and enjoyable.";
 
 var AboutMe = "I am a private maths tutor providing one-to-one tuition and support to all students from Key Stage 2 up to GCSE level, or to adult learners who need to improve their maths skills for further education or career opportunities.<br><br>I’ve been teaching and mentoring Maths for over 15 years. I started tutoring for fun but, as I had such good results with my initial pupils it attracted so many more.  This inspired me to do the job professionally.  I cover all essential topics at Key Stage 2, Key Stage 3 and Key Stage 4 including preparing pupils for 11+, 13+ and GCSE exams. I also teach adult learners, who may be struggling with a maths exam on a higher education course, wishing to retake a maths GCSE or grappling with numerical reasoning in the workplace or for a job interview.<br><br>I love showing the fun side of maths and demonstrating that a problem isn’t just a set of terrifying numbers but an enjoyable challenge!  Working one-to-one, I concentrate on specific areas to build pupils’ self-confidence and understanding to improve results. Initially I assess each individual’s level and need and encourage them to understand the basics and use their own common sense to simplify more complex problems.  I enjoy teaching all standards; fine tuning to achieve an A* or helping a pupil who is really struggling with everything.<br><br>I can provide one-off lessons or regular weekly slots.  Regular students will benefit from tailormade study plans, homework, ongoing mini tests and exam preparation as part of the learning package.<br><br>I am a member of The Tutor's Association and have a DBS Certificate.";
@@ -53,18 +38,23 @@ switch(Requested)
 //The Function that deals with the colour in the Background
 function Colour()
 {
+  //Gets the time
   const Time = new Date();
 
+  //splits the time into different section so it can be used
   var mins = Time.getMinutes();
   var hrs = Time.getHours();
   var CurrentDate = Time.getDate();
   var month = Time.getMonth();
   
+  //Uses the mins variable to set the hue of the background image
   var Hue = 6*mins;
+  //Uses the hrs and mins (My maths Teacher helped me with the Maths behind the brightness)
   var Brightness = 40*Math.pow(Math.sin(hrs*3+mins), 2)+30;
   
   console.log(Hue, Brightness);
   
+  //sets the css variables Hue and Brightness to the values of the JS Variables Hue and Brightness
   document.documentElement.style.setProperty("--Hue", Hue);
   document.documentElement.style.setProperty("--Brightness", Brightness);
 }
